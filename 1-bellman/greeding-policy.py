847ffc2cf526f848d5aa90e30d225d78713decf3
Shinichi Yoshinaga <61480734+Shinichi0713@users.noreply.github.com>
2024-11-11 06:30:49 +0900

    Add files via upload

--- 1-bellman/greeding-policy.py
+++ 1-bellman/greeding-policy.py
@@ -17,4 +17,35 @@ r[2, 1, 0] = 0.0
 r[2, 2, 1] = -1.0
 
 
+# value function
+v = [0, 0, 0]
+v_prev = copy.copy(v)
 
+# value iteration
+q = np.zeros((3, 2))
+
+# 方策分布初期化
+pi = [0.5, 0.5, 0.5]
+
+# 方策評価関数
+def policy_evaluation(pi, p, r, gamma):
+    # init
+    R = [0, 0, 0]
+    P = np.zeros((3, 3))
+    A = np.zeros((3, 3))
+
+    for i in range(3):
+        P[i, i] = 1 - pi[i]
+        P[i, (i + 1) % 3] = p[i] * pi[i]
+        P[i, (i + 2) % 3] = (1 - p[i]) * pi[i]
+
+        # 報酬ベクトル
+        R[i] = pi[i] * (pi[i] * r[i, (i+1)%3,0]+ \
+                (1-pi[i]) * r[i, (i+2)%3,0]) + \
+                (1-pi[i]) * r[i, i, 1]
+        
+        # ベルマンの解
+        A = np.eye(3) - gamma * P
+        B = np.linalg.inv(A)
+        v_sol = np.dot(B, R)
+    return v_sol
